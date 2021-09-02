9b7ef3335220c76cf28d2909f334c591cc02abd2
lvillen <l.villenjaldon@gmail.com>
2021-09-02 13:35:46 +0200

    Creates example unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+describe('Example Component', () => {
+  test('Should be more than 10', () => {
+    //Arrange
+    let value = 9;
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+    //Act
+    value += 2;
+
+    //Assert
+    expect(value).toBeGreaterThan(10);
+  });
+});
